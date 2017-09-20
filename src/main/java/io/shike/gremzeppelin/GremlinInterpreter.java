6407e4f5f826572bc5577084b67ec2d720f9abe8
Ranger Tsao <cao.zhifu@gmail.com>
2017-09-20 18:32:54 +0800

    [WIP] simple impl `Interpreter`
    TODO:
    1. gremlin-server configurations
    2. extract `ResultSet`

--- src/main/java/io/shike/gremzeppelin/GremlinInterpreter.java
+++ src/main/java/io/shike/gremzeppelin/GremlinInterpreter.java
@@ -1,15 +1,36 @@
 package io.shike.gremzeppelin;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonPrimitive;
+
+import org.apache.tinkerpop.gremlin.driver.Client;
+import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.ResultSet;
 import org.apache.zeppelin.interpreter.Interpreter;
 import org.apache.zeppelin.interpreter.InterpreterContext;
 import org.apache.zeppelin.interpreter.InterpreterResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+
+import io.vavr.control.Try;
 
 /**
  * @author Ranger Tsao(https://github.com/boliza)
  */
 public class GremlinInterpreter extends Interpreter {
+
+    private static final Logger logger = LoggerFactory.getLogger(GremlinInterpreter.class);
+
+    private static final String DEFAULT_GREMLIN_SERVER_HOSTS = "127.0.0.1";
+    private static final int DEFAULT_GREMLIN_SERVER_PORT = 8182;
+
+    private String GREMLIN_SERVER_HOSTS = "gremlin.server.hosts";
+    private String GREMLIN_SERVER_PORT = "gremlin.server.port";
+
+    private Client client;
+    private Cluster cluster;
 
     public GremlinInterpreter(Properties property) {
         super(property);
@@ -17,27 +38,44 @@ public class GremlinInterpreter extends Interpreter {
 
     @Override
     public void open() {
-
+        logger.info("init gremlin client via {}", property);
+        cluster = Cluster.build()
+                         .addContactPoints(property.getProperty(GREMLIN_SERVER_HOSTS, DEFAULT_GREMLIN_SERVER_HOSTS).split(","))
+                         .port(Try.of(() -> Integer.parseInt(property.getProperty(GREMLIN_SERVER_PORT))).getOrElse(DEFAULT_GREMLIN_SERVER_PORT))
+                         .create();
+        client = cluster.connect();
     }
 
     @Override
     public void close() {
-
+        client.close();
+        cluster.close();
     }
 
     @Override
-    public InterpreterResult interpret(String s, InterpreterContext interpreterContext) {
-        return null;
+    public InterpreterResult interpret(String gremlin, InterpreterContext interpreterContext) {
+        logger.info("execute gremlin traversal {}", gremlin);
+        try {
+            ResultSet results = client.submit(gremlin);
+            JsonArray array = results.stream()
+                                     .map(result -> new JsonPrimitive(results.toString()))
+                                     .collect(JsonArray::new, JsonArray::add, JsonArray::addAll);
+
+            //TODO extract ResultSet
+            return new InterpreterResult(InterpreterResult.Code.SUCCESS, array.toString());
+        } catch (RuntimeException e) {
+            return new InterpreterResult(InterpreterResult.Code.ERROR, e.getMessage());
+        }
     }
 
     @Override
     public void cancel(InterpreterContext interpreterContext) {
-
+        //do nothing
     }
 
     @Override
     public FormType getFormType() {
-        return null;
+        return FormType.SIMPLE;
     }
 
     @Override
